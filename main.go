7a14c88d4478812ea9b6608ba55df6093dbc597c
josemasster <joseh.jgvr55@gmail.com>
2026-03-05 16:14:14 -0500

    Se agregan cors

--- main.go
+++ main.go
@@ -4,12 +4,29 @@ import (
 	_ "api_mid_sabaticos/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 	"github.com/udistrital/utils_oas/errorhandler"
 	"github.com/udistrital/utils_oas/xray"
 )
 
+// CORS middleware
+var CORS = func(ctx *context.Context) {
+	ctx.Output.Header("Access-Control-Allow-Origin", "*")
+	ctx.Output.Header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+	ctx.Output.Header("Access-Control-Allow-Headers", "Content-Type,Authorization,Accept,X-Requested-With")
+	ctx.Output.Header("Access-Control-Allow-Credentials", "true")
+
+	if ctx.Input.Method() == "OPTIONS" {
+		ctx.Output.SetStatus(200)
+		return
+	}
+}
+
 func main() {
 	xray.InitXRay()
+
+	// Insertar filtro CORS
+	beego.InsertFilter("*", beego.BeforeRouter, CORS)
 
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
